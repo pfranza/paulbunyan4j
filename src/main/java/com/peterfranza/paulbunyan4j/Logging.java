4eef4fc1bfb935998ad60ab74e934f0e3a54a9e7
pfranza <pfranza@gmail.com>
2014-01-31 10:11:30 -0500

    Creation of modules

---
+++ src/main/java/com/peterfranza/paulbunyan4j/Logging.java
@@ -0,0 +1,5 @@
+package com.peterfranza.paulbunyan4j;
+
+public interface Logging {
+
+}
---
+++ src/main/java/com/peterfranza/paulbunyan4j/modules/LoggingClientModule.java
@@ -0,0 +1,13 @@
+package com.peterfranza.paulbunyan4j.modules;
+
+import com.google.inject.AbstractModule;
+
+public class LoggingClientModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
